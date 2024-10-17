20fde7cdefcca6f4dde4b2025678b7647cb9fdbb
Muhammad Ichsan Fadillah <muhammad.ichsan.fadillah@mhs.politala.ac.id>
2024-10-17 09:43:31 +0800

    init isan jr

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route for the Home page
 Route::get('/', function () {
-    return view('welcome');
+    return view('home'); // The 'home' view will be loaded
+});
+
+// Route for the About page
+Route::get('/about', function () {
+    return view('about'); // The 'about' view will be loaded
+});
+
+// Route for the Test page
+Route::get('/test', function () {
+    return view('test');
 });
