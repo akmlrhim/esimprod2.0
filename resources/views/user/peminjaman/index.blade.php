e20d25a8275528c9aed52309031f9265c8df60e7
Muhammad Ichsan Fadillah <muhammad.ichsan.fadillah@mhs.politala.ac.id>
2024-11-11 15:06:20 +0800

    JavaScript Kuontol

--- resources/views/user/peminjaman/index.blade.php
+++ resources/views/user/peminjaman/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.user.master')
+@extends('layouts.user.main')
 
 @section('title', 'Peminjaman')
 
@@ -81,6 +81,7 @@
 
       <div class="flex items-center w-full space-x-3 md:w-auto">
 
+        <!-- Datepickers -->
         <div id="date-range-picker" date-rangepicker class="flex items-center">
           <div class="relative">
             <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -90,11 +91,11 @@
                   d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
               </svg>
             </div>
-            <input id="datepicker-range-start" name="start" type="text"
+            <input id="datepicker" type="text"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Pilih tanggal">
+              placeholder="Select date">
           </div>
-          <span class="mx-4 text-gray-500">sampai</span>
+          <span class="mx-4 text-gray-500">to</span>
           <div class="relative">
             <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
               <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
@@ -108,6 +109,7 @@
               placeholder="Pilih tanggal">
           </div>
         </div>
+
 
         <button id="dropdownRadioHelperButton" data-dropdown-toggle="dropdownRadioHelper"
           class="text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -271,12 +273,7 @@
       </div>
     </div>
   </div>
-
-
-
 </div>
-
-
 
 <div class="flex justify-center space-x-2 mt-4">
   <button type="button"
@@ -285,14 +282,19 @@
     class="text-white bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Simpan</button>
 </div>
 
-
 <script>
-// Set the minimum date for both start and end date inputs
-document.addEventListener("DOMContentLoaded", function() {
-  var today = new Date().toISOString().split('T')[0]; // Get today's date in YYYY-MM-DD format
-  document.getElementById("datepicker-range-start").setAttribute("min", today);
-  document.getElementById("datepicker-range-end").setAttribute("min", today);
+document.addEventListener('DOMContentLoaded', function() {
+  const datepickerInput = document.getElementById('datepicker');
+
+  // Initialize Flowbite's datepicker with minDate
+  const datepicker = new Datepicker(datepickerInput, {
+    minDate: new Date(), // Disable past dates
+    todayHighlight: true // Highlight today's date
+  });
 });
 </script>
+
+
+
 
 @endsection
--- resources/views/user/pengembalian/index.blade.php
+++ resources/views/user/pengembalian/index.blade.php
@@ -1,82 +1,105 @@
-@extends('layouts.user.master')
+@extends('layouts.user.main')
 
 @section('title', 'Pengembalian')
 
 @section('content')
 
-{{-- breadcrumb --}}
-<nav
-  class="flex px-3 py-3 text-gray-700 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700"
-  aria-label="Breadcrumb">
-  <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-    <li class="inline-flex items-center">
-      <a href="#"
-        class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-        <i class="fa-solid fa-house mr-3"></i> Home
-      </a>
-    </li>
-    <li aria-current="page">
-      <div class="flex items-center">
-        <i class="fa-solid fa-chevron-right"></i>
-        <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Flowbite</span>
-      </div>
-    </li>
-  </ol>
-</nav>
+{{-- datpicker --}}
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-4">
-  <div
-    class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900">
-    <div>
-      <button id="dropdownActionButton" data-dropdown-toggle="dropdownAction"
-        class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-        type="button">
-        Action
-        <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-          viewBox="0 0 10 6">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="m1 1 4 4 4-4" />
-        </svg>
-      </button>
+
+  <!-- <div class="relative max-w-sm">
+    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+      <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor" viewBox="0 0 20 20">
+        <path
+          d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+      </svg>
     </div>
+    <input id="datepicker-format" datepicker type="text"
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      placeholder="Select date">
+  </div> -->
+
+
+  <div id="date-range-picker" date-rangepicker class="flex items-center">
     <div class="relative">
-      <input type="text" id="table-search-users"
-        class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Search for users">
+      <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor" viewBox="0 0 20 20">
+          <path
+            d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+        </svg>
+      </div>
+      <input id="datepicker-format" datepicker-buttons datepicker-autoselect-today datepicker type="text"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Pilih tanggal">
+    </div>
+    <span class="mx-4 text-gray-500">Sampai</span>
+    <div class="relative">
+      <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor" viewBox="0 0 20 20">
+          <path
+            d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+        </svg>
+      </div>
+      <input id="datepicker-range-end" name="end" type="text"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Pilih tanggal">
     </div>
   </div>
 
-  <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-      <tr>
-        <th scope="col" class="p-4">
-          <input id="checkbox-all-search" type="checkbox"
-            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-        </th>
-        <th scope="col" class="px-6 py-3">No</th>
-        <th scope="col" class="px-6 py-3">Nama Barang</th>
-        <th scope="col" class="px-6 py-3">Merk</th>
-        <th scope="col" class="px-6 py-3">No Seri</th>
-        <th scope="col" class="px-6 py-3">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-        <td class="p-4">
-          <input id="checkbox-table-search-1" type="checkbox"
-            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-        </td>
-        <td class="px-6 py-4">1</td>
-        <td class="px-6 py-4">Laptop</td>
-        <td class="px-6 py-4">Apple</td>
-        <td class="px-6 py-4">12345678</td>
-        <td class="px-6 py-4">
-          <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-        </td>
-      </tr>
-      <!-- Additional rows as needed -->
-    </tbody>
-  </table>
+  <kbd
+    class="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500">Esc</kbd>
+
+  <button type="button"
+    class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Dark</button>
+
+  <!-- <script>
+  document.addEventListener('DOMContentLoaded', function() {
+    // Get the input element
+    const datepickerInput = document.getElementById('datepicker-format');
+
+    // Get today's date in MM/DD/YYYY format
+    const today = new Date();
+    const minDate = today.toLocaleDateString('en-US');
+
+    // Set the max date to one year ahead (for example)
+    const maxDate = new Date(today);
+    maxDate.setFullYear(today.getFullYear() + 1);
+    const maxDateStr = maxDate.toLocaleDateString('en-US');
+
+    // Set the min and max dates dynamically
+    datepickerInput.setAttribute('datepicker-min-date', minDate);
+    datepickerInput.setAttribute('datepicker-max-date', maxDateStr);
+  });
+  </script> -->
+
+
+  <script>
+  document.addEventListener('DOMContentLoaded', function() {
+    // Get the input element
+    const datepickerInput = document.getElementById('datepicker-format');
+
+    // Get today's date in MM/DD/YYYY format
+    const today = new Date();
+    const minDate = today.toLocaleDateString('en-US');
+
+    // Set the max date to today's date
+    const maxDate = today.toLocaleDateString('en-US');
+
+    // Set the min and max dates dynamically
+    datepickerInput.setAttribute('datepicker-min-date', minDate);
+    datepickerInput.setAttribute('datepicker-max-date', maxDate);
+
+    // Initialize the Flowbite datepicker (if necessary)
+    new Datepicker(datepickerInput, {
+      // The maxDate will prevent the user from selecting dates after today
+      maxDate: maxDate, // Disables future dates (i.e., dates after today)
+    });
+  });
+  </script>
 </div>
 
 @endsection
