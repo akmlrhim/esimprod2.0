cbadfb345c1b486fea19c6ca5679d9250030d15d
rahimmsz <150146538+rahimmsz@users.noreply.github.com>
2025-03-25 10:07:44 +0800

    Remove ignored files from repository

--- resources/js/app.js
+++
@@ -1,7 +0,0 @@
-import "./bootstrap";
-import "flowbite";
-import AOS from "aos";
-
-import $ from "jquery";
-window.$ = window.jQuery = $;
-AOS.init();
--- resources/views/components/head/tinymce-config.blade.php
+++
@@ -1,8 +0,0 @@
-<script src="{{ asset('vendor/tinymce/tinymce.min.js') }}" referrerpolicy="origin"></script>
-<script>
-  tinymce.init({
-    selector: 'textarea#editor',
-    plugins: 'table lists',
-    toolbar: 'undo redo| bold italic | alignleft aligncenter alignright | bullist numlist |'
-  });
-</script>
